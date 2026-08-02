a17177df2f2e6bd0fde3fa36baabbf31174e4caa
js-log <josephsinfield@yahoo.com>
2026-08-03 00:17:28 +0100

    Update template project basics for new project

--- buildimage.sh
+++ buildimage.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-image=jslog/devcontainer-node-base:${1:-latest}
+image=jslog/ledger-api:${1:-latest}
 
 docker build -t "$image" -f Dockerfile .
 docker push "$image"
--- runcontainer.ps1
+++ runcontainer.ps1
@@ -5,17 +5,17 @@ if ($param1 -ne "start" -and $param1 -ne "destructive") {
     exit 1
 }
 
-docker pull jslog/devcontainer-node-base:latest
+docker pull jslog/ledger-api:latest
 
 if ($param1 -eq "destructive") {
     Write-Host "Destroying the existing container and the /app volume with it. Any work not committed and pushed is lost."
 
-    $containers = docker ps -aq --filter volume=devcontainer-node-base
+    $containers = docker ps -aq --filter volume=ledger-api
     if ($containers) {
         docker rm -f $containers
     }
 
-    docker volume rm --force devcontainer-node-base
+    docker volume rm --force ledger-api
 }
 
 devcontainer up --remote-env GIT_USER_NAME=$(git config --get user.name) --remote-env GIT_USER_EMAIL=$(git config --get user.email) --workspace-folder .
